c96ad8b5c73c7ffd726c5d749741a5ca7fd20bfb
Samir Mahapatra <work.samir.mahapatra@gmail.com>
2024-10-04 14:58:16 +0000

    updated LSP plugin config and remove ensure_installed

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -8,24 +8,6 @@ require("mason").setup({})
 require("mason-lspconfig").setup({
 	-- Replace the language servers listed here
 	-- with the ones you want to install
-	ensure_installed = {
-		"tsserver",
-		"rust_analyzer",
-		"tailwindcss",
-		"prismals",
-		"markdown_oxide",
-		"lua_ls",
-		"java_language_server",
-		"html",
-		"gopls",
-		"ansiblels",
-		"angularls",
-		"yamlls",
-		"terraformls",
-		"sqlls",
-		"jedi_language_server",
-		"jqls",
-	},
 	handlers = {
 		lsp_zero.default_setup,
 	},
